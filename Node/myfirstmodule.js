843ab40b00bb515542544e06711bfc2461313c9a
qiusiyuan <truxihere@gmail.com>
2019-06-10 14:08:11 -0400

    electron

--- Node/myfirstmodule.js
+++
@@ -1,4 +0,0 @@
-//exports keyword to make properties and methods available outside the module file.
-exports.myDateTime = function () {
-  return Date();
-};
--- TRUXncrypter/config/config.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var configs = require('./config.json');
-
-
-module.exports.getconfig = function() {
-  configs.storage = configs.storage;
-  return configs;
-};
